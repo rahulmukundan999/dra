c3de4593b4773c15a3677bd141cd3a2d7fee9a01
rahul <rahulmukundan999@gmail.com>
2020-05-31 19:06:25 +0530

    Updates

---
+++ precache-manifest.2b1f7a27fee09243084358fe19c38cc2.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4290a56109ef213fd73c56cf4d53f3e",
+    "url": "/black-dashboard-react/index.html"
+  },
+  {
+    "revision": "a18c0a888cc176d34279",
+    "url": "/black-dashboard-react/static/css/2.2562d4f0.chunk.css"
+  },
+  {
+    "revision": "b6d8bf4a4eaeacd1b8c5",
+    "url": "/black-dashboard-react/static/css/main.4c8a26cc.chunk.css"
+  },
+  {
+    "revision": "a18c0a888cc176d34279",
+    "url": "/black-dashboard-react/static/js/2.846a8664.chunk.js"
+  },
+  {
+    "revision": "89654bf7202beb1fc56bea35fb833be7",
+    "url": "/black-dashboard-react/static/js/2.846a8664.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6d8bf4a4eaeacd1b8c5",
+    "url": "/black-dashboard-react/static/js/main.756d49c0.chunk.js"
+  },
+  {
+    "revision": "dc192b57bfed2afd6be1ea5558f17255",
+    "url": "/black-dashboard-react/static/js/main.756d49c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a45b1b270b0279ca8c7",
+    "url": "/black-dashboard-react/static/js/runtime-main.952b9ceb.js"
+  },
+  {
+    "revision": "bd6820f1a4015e9645e470ea8e7b1501",
+    "url": "/black-dashboard-react/static/media/anime3.bd6820f1.png"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/black-dashboard-react/static/media/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/black-dashboard-react/static/media/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/black-dashboard-react/static/media/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/black-dashboard-react/static/media/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "eb6be414b75c412c13eeee2d2e8390cb",
+    "url": "/black-dashboard-react/static/media/react-logo.eb6be414.png"
+  }
+]);
